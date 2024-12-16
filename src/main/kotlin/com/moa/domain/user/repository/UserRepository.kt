7cf0ccecc75201e45c4664eaff598c23bb48e45b
전민오 <jbj338033@gmail.com>
2024-12-16 13:26:45 +0900

    feat: user repository user type

--- src/main/kotlin/com/moa/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/moa/domain/user/repository/UserRepository.kt
@@ -2,7 +2,8 @@ package com.moa.domain.user.repository
 
 import com.moa.domain.user.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
 
-interface UserRepository: JpaRepository<User, Long> {
+interface UserRepository: JpaRepository<User, UUID> {
     fun findByDodamId(dodamId: String): User?
 }
